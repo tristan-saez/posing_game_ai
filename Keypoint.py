9a07cf33089c6fad448d514a6f535a9a2d4fda59
TrLSeysh <tristan.saez3@gmail.com>
2025-02-10 16:12:26 +0100

    Update Keypoint

---
+++ Keypoint.py
@@ -0,0 +1,36 @@
+class Keypoint:
+    """
+
+    Keypoint class
+
+    """
+
+    # "Nose": 0,
+    # "Neck": 1,
+    # "RShoulder": 2,
+    # "RElbow": 3,
+    # "RWrist": 4,
+    # "LShoulder": 5,
+    # "LElbow": 6,
+    # "LWrist": 7,
+    # "RHip": 8,
+    # "RKnee": 9,
+    # "RAnkle": 10,
+    # "LHip": 11,
+    # "LKnee": 12,
+    # "LAnkle": 13,
+    # "REye": 14,
+    # "LEye": 15,
+    # "REar": 16,
+    # "LEar": 17,
+    # "Background": 18
+    body_id = 0
+    x = 0
+    y = 0
+
+    def __init__(self, body_id=0, x=0, y=0):
+        self.id = id
+        self.x = x
+        self.y = y
+
+
