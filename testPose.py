d26c43651415b13a0acc95d6c55f4d21c13960e4
Momolog298 <momolog29@gmail.com>
2025-02-10 15:58:41 +0100

    Mise en fonctions des taches de détection du squelette

--- testPose.py
+++ testPose.py
@@ -2,80 +2,90 @@ import cv2 as cv
 import numpy as np
 import camera_functions as cf
 
-cam, size, out = cf.getCamera(write_video=False)
 
-BODY_PARTS = {"Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
-              "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
-              "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
-              "LEye": 15, "REar": 16, "LEar": 17, "Background": 18}
+def set_constants_pose():
+    cam, size, out = cf.getCamera(write_video=False)
 
-POSE_PAIRS = [["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
-              ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
-              ["Neck", "RHip"], ["RHip", "RKnee"], [
-    "RKnee", "RAnkle"], ["Neck", "LHip"],
-    ["LHip", "LKnee"], ["LKnee", "LAnkle"], [
-    "Neck", "Nose"], ["Nose", "REye"],
-    ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"]]
+    BODY_PARTS = {"Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
+                  "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
+                  "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
+                  "LEye": 15, "REar": 16, "LEar": 17, "Background": 18}
 
+    POSE_PAIRS = [["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
+                  ["RElbow", "RWrist"], ["LShoulder",
+                                         "LElbow"], ["LElbow", "LWrist"],
+                  ["Neck", "RHip"], ["RHip", "RKnee"], [
+        "RKnee", "RAnkle"], ["Neck", "LHip"],
+        ["LHip", "LKnee"], ["LKnee", "LAnkle"], [
+        "Neck", "Nose"], ["Nose", "REye"],
+        ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"]]
 
-image_width = size['width']
-image_height = size['height']
+    net = cv.dnn.readNetFromTensorflow("data/graph_opt.pb")
 
+    threshold = 0.2
 
-net = cv.dnn.readNetFromTensorflow("data/graph_opt.pb")
-
-threshold = 0.2
-
-ret, frame = cam.read()
-
-photo_height = frame.shape[0]
-photo_width = frame.shape[1]
-net.setInput(cv.dnn.blobFromImage(frame, 1.0, (image_width, image_height),
-             (127.5, 127.5, 127.5), swapRB=True, crop=False))
-
-out = net.forward()
-out = out[:, :19, :, :]
-
-assert (len(BODY_PARTS) == out.shape[1])
-
-points = []
-for i in range(len(BODY_PARTS)):
-    # Slice heatmap of corresponging body's part.
-    heatMap = out[0, i, :, :]
-
-    # Originally, we try to find all the local maximums. To simplify a sample
-    # we just find a global one. However only a single pose at the same time
-    # could be detected this way.
-    _, conf, _, point = cv.minMaxLoc(heatMap)
-    x = (photo_width * point[0]) / out.shape[3]
-    y = (photo_height * point[1]) / out.shape[2]
-    # Add a point if it's confidence is higher than threshold.
-    points.append((int(x), int(y)) if conf > threshold else None)
+    return cam, size, net, threshold, BODY_PARTS, POSE_PAIRS
 
 
-for pair in POSE_PAIRS:
-    partFrom = pair[0]
-    partTo = pair[1]
-    assert (partFrom in BODY_PARTS)
-    assert (partTo in BODY_PARTS)
+def boucle_pose(cam, size, net, threshold, BODY_PARTS, POSE_PAIRS):
+    ret, frame = cam.read()
 
-    idFrom = BODY_PARTS[partFrom]
-    idTo = BODY_PARTS[partTo]
+    photo_height = frame.shape[0]
+    photo_width = frame.shape[1]
+    net.setInput(cv.dnn.blobFromImage(frame, 1.0, (size['width'], size['height']),
+                                      (127.5, 127.5, 127.5), swapRB=True, crop=False))
 
-    if points[idFrom] and points[idTo]:
-        cv.line(frame, points[idFrom], points[idTo], (0, 255, 0), 3)
-        cv.ellipse(frame, points[idFrom], (3, 3), 0,
-                   0, 360, (0, 0, 255), cv.FILLED)
-        cv.ellipse(frame, points[idTo], (3, 3), 0,
-                   0, 360, (0, 0, 255), cv.FILLED)
+    out = net.forward()
+    out = out[:, :19, :, :]
 
-t, _ = net.getPerfProfile()
+    assert (len(BODY_PARTS) == out.shape[1])
 
-cv.imshow("Camera", frame)
+    points = []
+    for i in range(len(BODY_PARTS)):
+        # Slice heatmap of corresponging body's part.
+        heatMap = out[0, i, :, :]
 
-# waits for user to press any key
-# (this is necessary to avoid Python kernel form crashing)
-cv.waitKey(0)
+        # Originally, we try to find all the local maximums. To simplify a sample
+        # we just find a global one. However only a single pose at the same time
+        # could be detected this way.
+        _, conf, _, point = cv.minMaxLoc(heatMap)
+        x = (photo_width * point[0]) / out.shape[3]
+        y = (photo_height * point[1]) / out.shape[2]
+        # Add a point if it's confidence is higher than threshold.
+        points.append((int(x), int(y)) if conf > threshold else None)
 
-# closing all open windows
-cv.destroyAllWindows()
+    for pair in POSE_PAIRS:
+        partFrom = pair[0]
+        partTo = pair[1]
+        assert (partFrom in BODY_PARTS)
+        assert (partTo in BODY_PARTS)
+
+        idFrom = BODY_PARTS[partFrom]
+        idTo = BODY_PARTS[partTo]
+
+        if points[idFrom] and points[idTo]:
+            cv.line(frame, points[idFrom], points[idTo], (0, 255, 0), 3)
+            cv.ellipse(frame, points[idFrom], (3, 3), 0,
+                       0, 360, (0, 0, 255), cv.FILLED)
+            cv.ellipse(frame, points[idTo], (3, 3), 0,
+                       0, 360, (0, 0, 255), cv.FILLED)
+
+    t, _ = net.getPerfProfile()
+
+    cv.imshow("Camera", frame)
+
+    # waits for user to press any key
+    # (this is necessary to avoid Python kernel form crashing)
+    cv.waitKey(0)
+
+    # closing all open windows
+    cv.destroyAllWindows()
+
+
+def test_pose():
+    cam, size, net, threshold, BODY_PARTS, POSE_PAIRS = set_constants_pose()
+    boucle_pose(cam, size, net, threshold, BODY_PARTS, POSE_PAIRS)
+
+
+if __name__ == '__main__':
+    test_pose()
